1a4f9a7a162877530ea1835540813aa1098c47d0
Shweta Mishra <111523337+Shweta-Mishra-ai@users.noreply.github.com>
2026-03-23 18:06:00 +0530

    Feat :optimizer.py

--- app/services/optimizer.py
+++ app/services/optimizer.py
@@ -2,8 +2,13 @@ from openai import OpenAI
 from app.utils.prompt_loader import load_prompt
 import os
 import json
+import re
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def clean_json(text):
+    text = re.sub(r"```json|```", "", text).strip()
+    return json.loads(text)
 
 def optimize_article(article, keyword, geo):
     prompt = load_prompt("prompts/prompt2_geo_ai_optimization.txt")
@@ -20,4 +25,9 @@ def optimize_article(article, keyword, geo):
 
     content = response.choices[0].message.content
 
-    return json.loads(content)
+    try:
+        return clean_json(content)
+    except:
+        return {
+            "optimized_article_markdown": content
+        }
