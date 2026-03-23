12070dd6a5e3420af9d36dbf75f4dd209825b3c9
Shweta Mishra <111523337+Shweta-Mishra-ai@users.noreply.github.com>
2026-03-23 19:43:05 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
-def root():
-    return {"status": "WORKING"}
+def home():
+    return {"message": "Healthy Gut AI is running 🚀"}
+
 
 @app.get("/health")
 def health():
